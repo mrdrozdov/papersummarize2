5d698e670edecc5e1114e7e39abb17969c407599
Andrew Drozdov <andrew@mrdrozdov.com>
2018-04-15 18:41:05 -0400

    Add example db.

---
+++ papersummarize/papersummarize_db/tests/test_db.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class DBTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_db(self):
+        self.assertTrue(True)
